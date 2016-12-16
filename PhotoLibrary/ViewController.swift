9dc0c59f811d17ada8ddb31170519edea2ff77c1
DA SILVA André Henrique <andre.henrique@consultant.volvo.com>
2016-12-16 07:50:25 +0100

    Initial Commit

---
+++ PhotoLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoLibrary
+//
+//  Created by DA SILVA Andre Henrique on 16/12/2016.
+//  Copyright © 2016 a h e n q s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
